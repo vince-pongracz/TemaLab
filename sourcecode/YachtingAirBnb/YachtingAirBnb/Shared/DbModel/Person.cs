f0d9891cf06659ebfbef78cc6da73656d3f67744
Vince Pongracz <pongrvin@gmail.com>
2021-10-12 17:26:02 +0200

    DB classes created, User and authentication settings missing

---
+++ sourcecode/YachtingAirBnb/YachtingAirBnb/Shared/DbModel/Person.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YachtingAirBnb.Shared.DbModel
+{
+    public class Person : IdentityUser
+    {
+        public string FacebookLink { get; set; }
+        public string InstagramLink { get; set; }
+        public Person(string userName) : base(userName)
+        {
+            this.UserName = userName;
+        }
+    }
+}
---
+++ sourcecode/YachtingAirBnb/YachtingAirBnb/Shared/DbModel/Reservations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YachtingAirBnb.Shared.DbModel
+{
+    public class Reservations
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public int ShipId { get; set; }
+        public int PersonId { get; set; }
+    }
+}
